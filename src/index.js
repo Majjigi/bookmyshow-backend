45709e664537f8d45149ba9cd5310944a6f76a2a
Bharath M S <bharathmajjigi3@gmail.com>
2025-12-18 23:23:47 +0530

    register,login,change password handled

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import dotenv from "dotenv"
-import app from "./app.js"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
+  path: "./.env",
 });
 
 // Start the server only after DB connection is established
 // connectDB() method returns a promise which we can use to start the server
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8080, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
-
+  });
 
 // (async () => {
 //     try {
---
+++ src/models/userModel.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    emailID: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      emailID: this.emailID,
+      firstName: this.firstName,
+      lastName: this.lastName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
